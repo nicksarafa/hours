40a5f02e4b3506da1ecb1c2524c216a931d4bbd9
nsarafa <nicksarafa@gmail.com>
2016-11-01 16:42:30 -0400

    redux-actions init & re-write bird-actions to conform to redux-actions shoarthand

--- src/client/actions/bird-actions.js
+++ src/client/actions/bird-actions.js
@@ -1,11 +1,19 @@
-/* eslint-disable import/prefer-default-export */
-// define action type
+import { createAction } from 'redux-actions'
+
+// define & export action type
 export const MAKE_TALK = 'MAKE_TALK'
 
-// define action creator
-// action creator triggers MAKE_TALK action called makeTalk
-// includes type, and payload, per Flux Standard Action model
-export const makeTalk = () => ({
-  type: MAKE_TALK,
-  payload: true,
-})
+/**
+ * define & export action creator
+ * action creator triggers MAKE_TALK action called makeTalk
+ * includes type, and payload, per Flux Standard Action model
+ *
+ * redux-actions unlocks shorthand for generating Flux Standard Action model boilerplate
+ * action action is the same as the following..
+ *
+ * export const makeTalk = () => ({
+ *   type: MAKE_TALK,
+ *   payload: true,
+ * })
+ */
+export const makeTalk = createAction(MAKE_TALK, () => true)
