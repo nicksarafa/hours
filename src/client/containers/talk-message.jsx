df2092b113c43d8c484c343ee6d0d238177a90fa
nsarafa <nicksarafa@gmail.com>
2016-11-01 16:20:53 -0400

    rewrite bird-reducer as Immutable Map & update talk-message to get()

--- src/client/containers/talk-message.jsx
+++ src/client/containers/talk-message.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import Message from '../components/message'
 
 const mapStateToProps = state => ({
-  message: state.bird.hasTalked ? 'The bird has talked' : 'The bird is ruffling it feathers',
+  message: state.bird.get('hasTalked') ? 'The bird has talked' : 'The bird is ruffling it feathers',
 })
 
 export default connect(mapStateToProps)(Message)
--- src/client/reducers/bird-reducer.js
+++ src/client/reducers/bird-reducer.js
@@ -1,13 +1,14 @@
+import Immutable from 'immutable'
 import { MAKE_TALK } from '../actions/bird-actions'
 
-const initialState = {
+const initialState = Immutable.Map({
   hasTalked: false,
-}
+})
 
 const birdReducer = (state = initialState, action) => {
   switch (action.type) {
     case MAKE_TALK:
-      return { hasTalked: action.payload }
+      return state.set('hasTalked', action.payload)
     default:
       return state
   }
