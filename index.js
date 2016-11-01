780beddddbd432b141916c7d0f98395858e54156
nsarafa <nicksarafa@gmail.com>
2016-10-31 20:12:02 -0400

    remove color, move index to src directory, lib directory init

--- index.js
+++
@@ -1,7 +0,0 @@
-// init test
-console.log('hello, world')
-
-// Yarn pkg test
-const Color = require('color')
-redHexa = Color({r: 255, go: 0, b: 0}).hexString()
-console.log(redHexa)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const str = 'ES6'
+
+// ES6 test
+console.log(`Hello, ${str}`)
+
+// init test
+console.log('🌎')
