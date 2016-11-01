859518c0d7a0e7bc593b1a5e1ea3b556e3cb5b0f
nsarafa <nicksarafa@gmail.com>
2016-11-01 19:36:57 -0400

    flow via gulp-flow, .flowconfig  init & update lint gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ import del from 'del'
 import eslint from 'gulp-eslint'
 import webpack from 'webpack-stream'
 import mocha from 'gulp-mocha'
+import flow from 'gulp-flowtype'
 import webpackConfig from './webpack.config.babel'
 
 const paths = {
@@ -60,4 +61,5 @@ gulp.task('lint', () => {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
+    .pipe(flow({ abort: true }))
 })
