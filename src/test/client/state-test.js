673b3cbc707936ff911cc07c4265378ffebf3a9b
nsarafa <nicksarafa@gmail.com>
2016-11-01 17:33:13 -0400

    chai, mocha, src/test directory, src/test/client/state-test file init

---
+++ src/test/client/state-test.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-extraneous-dependencies, no-unused-expressions */
+
+import { createStore } from 'redux'
+import { combineReducers } from 'react-immutable'
+import { should } from 'chai'
+import { descripe, it, beforeEach } from 'mocha'
+import { birdReducer } from '../../client/reducers/bird-reducer'
+import { makeTalk } from '../../client/actions/bird-actions'
+
+should()
+let store()
+
+describe('App State', () => {
+  describe('Bird', () => {
+    beforeEach(() => {
+      store = createStore(combineReducers({
+        bird: birdReducer,
+      }))
+    })
+  })
+  describe('makeTalk', () => {
+    it('should make hasTalked go from false to true', () => {
+      store.getState().getIn(['bird', 'hasTalked']).should.be.false
+      store.dispatch(makeTalk())
+      store.getState().getIn(['bird', 'hasTalked']).should.be.true
+    })
+  })
+})
