57fc6d7e7bab3e4a5936d444b099fdd6b587b02f
nsarafa <nicksarafa@gmail.com>
2016-11-13 16:45:53 +0700

    consolidate Hamburger styles

--- src/client/components/hamburger.jsx
+++ src/client/components/hamburger.jsx
@@ -10,27 +10,22 @@ import React, { PropTypes } from 'react'
 
 const style = {
   container: {
-    alignItems: 'space-between',
     display: 'flex',
     flexDirection: 'column',
+    height: 55,
+    justifyContent: 'space-between',
     width: 55,
   },
   layer: {
     backgroundColor: '#C0C0C0',
     height: 5,
-    width: 'inherit',
-  },
-  break: {
-    height: 20,
   },
 }
 
 const Hamburger = () => (
   <div style={style.container}>
     <div style={style.layer} />
-    <div style={style.break} />
     <div style={style.layer} />
-    <div style={style.break} />
     <div style={style.layer} />
   </div>
 )
