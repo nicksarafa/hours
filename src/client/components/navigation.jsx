94f4a265d7e57a8b7f3b02faa226f10639426526
nsarafa <nicksarafa@gmail.com>
2016-11-15 13:23:57 +0700

    replace Hamburger Navigation component with appropriate container

--- src/client/components/navigation.jsx
+++ src/client/components/navigation.jsx
@@ -6,46 +6,37 @@
  * @interface
  */
 
-import React, { Component, PropTypes } from 'react'
-import Hamburger from './hamburger'
+import React, { PropTypes } from 'react'
+import Hamburger from '../containers/hamburger-button'
 import Logo from './logo'
 import Link from './link'
 
-class Navigation extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isHidden: false,
-    }
+function Navigation() {
+  const style = {
+    container: {
+      alignItems: 'flex-end',
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'nowrap',
+      justifyContent: 'space-between',
+      marginTop: 21,
+    },
+    subWrap: {
+      marginBottom: 13,
+    },
   }
 
-  render() {
-    const style = {
-      container: {
-        alignItems: 'flex-end',
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'nowrap',
-        justifyContent: 'space-between',
-        marginTop: 21,
-      },
-      subWrap: {
-        marginBottom: 13,
-      },
-    }
-
-    return (
-      <div style={style.container}>
-        <div style={style.subWrap}>
-          <Hamburger isOpen={this.state.isHidden} />
-        </div>
-        <Logo color="" />
-        <div style={style.subWrap}>
-          <Link linkLabel="Hire Me" />
-        </div>
+  return (
+    <div style={style.container}>
+      <div style={style.subWrap}>
+        <Hamburger />
       </div>
-    )
-  }
+      <Logo color="" />
+      <div style={style.subWrap}>
+        <Link linkLabel="Contact" />
+      </div>
+    </div>
+  )
 }
 
 Navigation.propTypes = {
