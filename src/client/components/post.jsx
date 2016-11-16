b13513e2f3571fcd8e13bbbc3bfaaad8f90933fd
nsarafa <nicksarafa@gmail.com>
2016-11-16 17:08:49 +0700

    Post backDrop init

--- src/client/components/post.jsx
+++ src/client/components/post.jsx
@@ -1,9 +1,17 @@
+/**
+ * @export Post
+ * @description blog post with cover, date, and background coverPhoto
+ * @todo update default with static image instead of placehold.it link
+ * @todo fix failed propTypes console log errors
+ */
 import React, { PropTypes } from 'react'
 import moment from 'moment'
 
-function Post({ date, title }) {
+const Post = ({ backDrop, date, title }) => {
+  const backdrop = { backgroundImage: backDrop }
+
   return (
-    <div>
+    <div style={backdrop}>
       <h3>{title}</h3>
       <h5>{date}</h5>
     </div>
@@ -11,11 +19,13 @@ function Post({ date, title }) {
 }
 
 Post.propTypes = {
-  date: PropTypes.func.isRequired,
+  backDrop: PropTypes.instanceOf(Image).isRequired,
+  date: PropTypes.instanceOf(Date).isRequired,
   title: PropTypes.string.isRequired,
 }
 
 Post.defaultProps = {
+  backDrop: 'url(http://placehold.it/350x150)',
   date: moment().startOf('day').fromNow(),
   title: 'The Gunpowder Treason',
 }
