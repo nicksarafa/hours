c2a8eb25948b3de34b0f752fc6c2ba628341ae7d
nsarafa <nicksarafa@gmail.com>
2016-11-16 15:55:53 +0700

    remove legacy components

--- src/client/containers/talk-message.jsx
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import Message from '../components/message'
-
-const mapStateToProps = state => ({
-  message: state.getIn(['bird', 'hasTalked']) ? 'The bird has talked' : 'The bird is ruffling it feathers',
-})
-
-export default connect(mapStateToProps)(Message)
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,4 @@
 /* eslint-disable no-console */
 
-import Test from '../shared/bird'
-
-const zazu = new Test('Zazu')
-
-// es6 test
-console.log(zazu.talk())
-
 // world init test
 console.log('🌎')
